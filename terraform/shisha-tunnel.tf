6e59ab8ceb2339a3658b6e07ba67b23940fcf51a
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-03-26 14:47:20 +0300

    feat:new tunnel

---
+++ terraform/shisha-tunnel.tf
@@ -0,0 +1,87 @@
+resource "random_bytes" "shisha_tunnel_secret" {
+  length = 32
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "shisha" {
+  account_id    = local.cloudflare_account_id
+  name          = "shisha-new"
+  config_src    = "cloudflare"
+  tunnel_secret = random_bytes.shisha_tunnel_secret.base64
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "shisha" {
+  account_id = local.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.shisha.id
+
+  config = {
+    ingress = [
+      {
+        hostname = "photos.m1xxos.online"
+        service  = "https://192.168.1.128"
+        origin_request = {
+          origin_server_name = "photos.m1xxos.online"
+        }
+      },
+      {
+        hostname = "cloud.m1xxos.online"
+        service  = "https://192.168.1.128"
+        origin_request = {
+          origin_server_name = "cloud.m1xxos.online"
+        }
+      },
+      {
+        hostname = "finn.m1xxos.online"
+        service  = "https://192.168.1.128"
+        origin_request = {
+          origin_server_name = "finn.m1xxos.online"
+        }
+      },
+      {
+        hostname = "music.m1xxos.online"
+        service  = "https://192.168.1.128"
+        origin_request = {
+          origin_server_name = "music.m1xxos.online"
+        }
+      },
+      {
+        service = "http_status:404"
+      }
+    ]
+  }
+}
+
+resource "cloudflare_dns_record" "photos" {
+  zone_id = local.cloudflare_zone_id
+  name    = "photos"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.shisha.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "cloud" {
+  zone_id = local.cloudflare_zone_id
+  name    = "cloud"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.shisha.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "finn" {
+  zone_id = local.cloudflare_zone_id
+  name    = "finn"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.shisha.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "music" {
+  zone_id = local.cloudflare_zone_id
+  name    = "music"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.shisha.id}.cfargotunnel.com"
+  proxied = true
+  ttl     = 1
+}
